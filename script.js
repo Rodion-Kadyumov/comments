0dbd317374a8e560489718ba6b248fcd4e2bef60
Rodion Kadyumov <krrkes@gmail.com>
2023-03-11 16:02:13 +0500

    Внешность

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let comments = [];
 loadComments();
-// deleteComments();
 
 document.getElementById('comment-add').onclick = function(){
   let commentName = document.getElementById('comment-name');
@@ -29,23 +28,34 @@ function loadComments(){
   showComments();
 }
 
-// function deleteComments() {
-//   document.getElementById('delete').onclick = function() {
-//     delete localStorage('comments')
-//   }
-// }
+let btn = document.querySelector('#delete');
+
 
 function showComments (){
   let commentField = document.getElementById('comment-field');
-  let out = '';
+  let out = '<div>';
   comments.forEach(function(item) {
-    out += `<p class="delete"><span id="delete">X</span></p>`;
-    out += `<p class="data"><em>${timeConverter(item.time)}</em></p>`;
-    out += `<p class="name" role="alert"><b>${item.name}</b></p>`;
-    out += `<p class="commit" role="alert">${item.body}</p>`;
+    out += `<div class="wrap">
+    <div class="head">
+    <p class="data"><em>${timeConverter(item.time)}</em></p>`;
+    out += `<p class="delete" id="delete"><span>X</span></p>
+    </div>`;
+    out += `<div class="body">
+      <p class="name" role="alert"><b><img src="/images/avatar.png" alt="Аатар">${item.name}</b></p>`;
+    out += `<p class="commit" role="alert">${item.body}</p>
+    </div>`;
+    out += `<div class="footer">
+        <img src="/images/filled-like.png" alt="">
+      </div>
+    </div>`;
   });
-  commentField.innerHTML =`<li>${out}</li>`;
+  out += '</div>'
+  commentField.innerHTML = out;
 }
+
+btn.addEventListener('click', () => {
+  localStorage.removeItem('comments')
+})
 
 function timeConverter(UNIX_timestamp){
     let a = new Date(UNIX_timestamp * 1000);
@@ -59,3 +69,23 @@ function timeConverter(UNIX_timestamp){
     let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
     return time;
   }
+
+  
+// const likeBtn = document.querySelectorAll(".like__btn");
+// let likeIcon = document.querySelectorAll("#icon"),
+// count = document.querySelectorAll("#count");
+
+// let clicked = false;
+
+
+// likeBtn.addEventListener("click", () => {
+//   if (!clicked) {
+//     clicked = true;
+//     likeIcon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+//     count.textContent++;
+//   } else {
+//     clicked = false;
+//     likeIcon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+//     count.textContent--;
+//   }
+// });
